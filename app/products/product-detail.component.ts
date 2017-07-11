774d8e3a4c05c8f47fec204885ade85a3a750413
Ranwala, Ravindra <Ravindra.Ranwala@pearson.com>
2017-07-11 21:12:14 +0530

    product details component was completed.

--- app/products/product-detail.component.ts
+++ app/products/product-detail.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-
+import { ProductService } from './product.service';
 import { IProduct } from './product';
+
+import { Subscription }       from 'rxjs/Subscription';
 
 @Component({
     templateUrl: 'app/products/product-detail.component.html'
@@ -9,13 +11,29 @@ import { IProduct } from './product';
 export class ProductDetailComponent implements OnInit{
     pageTitle: string = 'Product Detail';
     product: IProduct;
+    errorMessage: string;
+    private sub: Subscription;
 
     constructor(private _route: ActivatedRoute,
-                private _router: Router) {}
+                private _router: Router, 
+                private _productService: ProductService) {}
 
     ngOnInit(): void {
-        let id = +this._route.snapshot.params['id'];
-        this.pageTitle += `: ${id}`;
+        this.sub = this._route.params.subscribe(
+            params => {
+                let id = +params['id'];
+                this.getProduct(id);
+            });
+    }
+
+    ngOnDestroy() {
+        this.sub.unsubscribe();
+    }
+
+    getProduct(id: number) {
+        this._productService.getProduct(id).subscribe(
+            product => this.product = product,
+            error => this.errorMessage = <any>error);
     }
 
     onBack(): void {
