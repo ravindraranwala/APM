f8ac4450291d560820f840f623cab13cdcdc77f4
Ranwala, Ravindra <Ravindra.Ranwala@pearson.com>
2017-07-12 17:32:14 +0530

    app was structured into cohesive modules.

---
+++ app/products/product.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { ProductListComponent } from './product-list.component';
+import { ProductDetailComponent } from './product-detail.component';
+import { ProductFilterPipe } from './product-filter.pipe';
+import { ProductService } from './product.service';
+import { ProductDetailGuard } from './product-guard.service';
+
+import { SharedModule } from '../shared/shared.module';
+
+@NgModule({
+    declarations: [
+        ProductListComponent,
+        ProductDetailComponent,
+        ProductFilterPipe,
+    ],
+    imports: [
+        SharedModule,
+        RouterModule.forChild([
+            { path: 'products', component: ProductListComponent },
+            { path: 'product/:id',
+                canActivate: [ProductDetailGuard], 
+                component: ProductDetailComponent },
+        ]),
+    ],
+    providers: [
+        ProductDetailGuard,
+        ProductService
+    ]
+})
+export class ProductModule {}
